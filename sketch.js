78416c2c3a290d423950e61cbf4c1afca071770c
MartinBinu <68335599+MartinBinu@users.noreply.github.com>
2020-10-13 13:28:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ground,paperball,dustbin1,dustbin2,dustbin3;
+
+function preload()
+{
+	dustbin = loadImage("dustbingreen.png");
+}
+
+function setup() {
+   createCanvas(2000, 800);
+   engine = Engine.create();
+   world = engine.world;
+   ground = new Ground(600,350,1200,10);
+   paperball = new PaperBall(200,340);
+   dustbin1 = new Dustbin(1100,245,20,200,);
+   dustbin2 = new Dustbin(990,335,200,20);
+   dustbin3 = new Dustbin(880,245,20,200);
+   //Create the Bodies Here.
+   Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  ground.display();
+  paperball.display();
+  dustbin1.display();
+  dustbin2.display();
+  dustbin3.display();
+  drawSprites();
+}
+
+function keyPressed(){
+
+	if(keyCode === UP_ARROW){
+    Matter.Body.applyForce(paperball.body,paperball.body.position,{x: 120,y: -120});
+    //1000,145,150,200
+	}
+   
+ }
